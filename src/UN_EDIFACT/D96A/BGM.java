81f3700beab2b9bacb7ffe04d08d1cbe4ac25fb1
BohseOnkel63 <ilkka.mannelin@iki.fi>
2015-05-22 15:27:07 +0300

    New Segment class

--- src/UN_EDIFACT/D96A/BGM.java
+++ src/UN_EDIFACT/D96A/BGM.java
@@ -5,8 +5,10 @@
  */
 package UN_EDIFACT.D96A;
 
-import UN_EDIFACT.EdiFunctions;
-import UN_EDIFACT.ValidityException;
+import UN_EDIFACT.Element;
+import UN_EDIFACT.Segment;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * UN-EDIFACT
@@ -14,76 +16,28 @@ import UN_EDIFACT.ValidityException;
  * 
  * @author mannelini
  */
-public class BGM {
-    private final String Tag = "BGM";
-    private final String Description = "BEGINNING OF MESSAGE";
-    private boolean Mandatory;
+public class BGM extends Segment {
     
-    /**
-     * Composite element C002
-     */
     public C002 eC002;
-
-    /**
-     *
-     */
     public E1004 e1004;
-
-    /**
-     *
-     */
     public E1225 e1225;
-
-    /**
-     *
-     */
     public E4343 e4343;
 
     /**
      *
      */
     public BGM() {
-        this(false, null, null, null, null, null, null, null);
+        this(false);
     }
     
     /**
      *
      * @param Mandatory
-     * @param s1001
-     * @param s1131
-     * @param s3055
-     * @param s1000
-     * @param s1004
-     * @param s1225
-     * @param s4343
      */
-    public BGM(Boolean Mandatory, String s1001, String s1131, String s3055, String s1000, String s1004, String s1225, String s4343) {
-        this.Mandatory = Mandatory;
-        eC002 = new C002(false, s1001, s1131, s3055, s1000);
-        e1004 = new E1004(s1004);
-        e1225 = new E1225(s1225);
-        e4343 = new E4343(s4343);
+    public BGM(Boolean Mandatory) {
+        super("BGM", "BEGINNING OF MESSAGE", "To indicate the type and function of a message and to transmit the identifying number.");
+        this.setMandatory(Mandatory);
+        super.setElementList((ArrayList) Arrays.asList(new Object[]{eC002, e1004, e1225, e4343}));
     }
 
-    /**
-     *
-     * @throws ValidityException
-     */
-    public void validate() throws ValidityException {
-        try {
-            eC002.validate();
-            e1004.validate();
-            e1225.validate();
-            e4343.validate();
-        } catch (ValidityException ex) {
-            throw new ValidityException(this.Tag + "/" + ex.getMessage());
-        }
-    }
-
-    @Override
-    public String toString() {
-        String output;
-        output = this.Tag + "+" + eC002.toString() + "+" + e1004.toString() + "+" + e1225.toString() + "+" + e4343.toString();
-        return EdiFunctions.trim(output) + "'";
-    }
 }
---
+++ src/UN_EDIFACT/Segment.java
@@ -0,0 +1,138 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package UN_EDIFACT;
+
+import java.util.ArrayList;
+
+
+/**
+ *
+ * @author mannelini
+ */
+public class Segment {
+    private String Tag;
+    private String Description;
+    private String LongDescription;
+
+    private Boolean Mandatory;
+    private ArrayList ElementList;
+
+    public Segment(String Tag, String Description, String LongDescription) {
+        this.Tag = Tag;
+        this.Description = Description;
+        this.LongDescription = LongDescription;
+        this.ElementList = new ArrayList<>();
+    }
+
+    public void addElement(Object O) {
+        ElementList.add(O);
+    }
+
+    @Override
+    public String toString() {
+        String output;
+        output = "";
+        Element e;
+        CompositeElement ce;
+        
+        e = null;
+        ce = null;
+        
+        System.out.println("ElementList.size()=" + ElementList.size());
+        for (Object o : ElementList ) {
+            if (o.getClass().isInstance(e)) {
+                e = (Element) o;
+                if (ElementList.indexOf(o) == 0) {
+                    output = e.toString() + "+";
+                } else {
+                    output = output + e.toString() + "+";
+                }
+            } 
+            if (o.getClass().isInstance(ce)) {
+                ce = (CompositeElement) o;
+                if (ElementList.indexOf(o) == 0) {
+                    output = ce.toString() + "+";
+                } else {
+                    output = output + ce.toString() + "+";
+                }
+            }
+        }
+        return EdiFunctions.trim(output);
+    }
+    
+    
+    public void validate() throws ValidityException {
+        Element e;
+        CompositeElement ce;
+        
+        e = null;
+        ce = null;
+        
+        try {
+            for (Object o : ElementList ) {
+                if (o.getClass().isInstance(e)) {
+                    e = (Element) o;
+                    e.validate();
+                }
+                if (o.getClass().isInstance(ce)) {
+                    ce = (CompositeElement) o;
+                    ce.validate();
+                }
+            }
+        } catch(ValidityException ex) {
+            throw new ValidityException(this.getTag() + "." + ex.getMessage());
+        }
+    }
+    
+    /*************************************************************************/
+    /* Getters and Setters                                                   */
+    /*************************************************************************/
+
+    /**
+     * 
+     * @return  
+     */
+    public String getTag() {
+        return Tag;
+    }
+
+    public void setTag(String Tag) {
+        this.Tag = Tag;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String Description) {
+        this.Description = Description;
+    }
+
+    public String getLongDescription() {
+        return LongDescription;
+    }
+
+    public void setLongDescription(String LongDescription) {
+        this.LongDescription = LongDescription;
+    }
+
+    public Boolean getMandatory() {
+        return Mandatory;
+    }
+
+    public void setMandatory(Boolean Mandatory) {
+        this.Mandatory = Mandatory;
+    }
+
+    public ArrayList getElementList() {
+        return ElementList;
+    }
+
+    public void setElementList(ArrayList ElementList) {
+        this.ElementList = ElementList;
+    }
+
+}
